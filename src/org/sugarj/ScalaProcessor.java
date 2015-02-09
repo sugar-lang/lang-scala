2029d7e2f969a7a03966fa45e06bc6dae505f845
Sebastian Erdweg <seba.github@gmail.com>
2015-02-09 14:04:59 +0100

    refactor

--- src/org/sugarj/ScalaProcessor.java
+++ src/org/sugarj/ScalaProcessor.java
@@ -10,8 +10,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.sugarj.baselang.IORelay;
 import org.sugarj.common.ATermCommands;
-import org.sugarj.common.Environment;
 import org.sugarj.common.FileCommands;
 import org.sugarj.common.StringCommands;
 import org.sugarj.common.path.Path;
@@ -76,7 +76,7 @@ public class ScalaProcessor extends AbstractBaseProcessor {
    * processing stuff follows here
    */
   @Override
-  public void init(Set<RelativePath> sourceFiles, Environment environment) {
+  public void init(Set<RelativePath> sourceFiles, IORelay environment) {
     if (sourceFiles.size() != 1)
       throw new IllegalArgumentException("Fomega can only compile one source file at a time.");
     
